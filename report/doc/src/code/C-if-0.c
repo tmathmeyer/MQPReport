208702990f42d1f19e6462025f24f200c0ff40e5
Ted Meyer <tmathmeyer@gmail.com>
2015-11-22 21:06:12 -0500

    fixed lauer's comments

--- report/doc/src/code/C-if-0.c
+++ report/doc/src/code/C-if-0.c
@@ -1,9 +1,10 @@
 #if 0
 This code is a comment
 despite the fact that there are no // or /* */
-
+Student name: Ethel L. Cahall
 #endif
 
+#include <stdio.h>
 int main() {
     printf("hello world\n");
 }
